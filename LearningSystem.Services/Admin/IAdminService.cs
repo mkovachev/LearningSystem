9e2f9cdc445c6d1808ec0a3bd107c83cfe0c424c
mkovachev <martin.kovachev@outlook.com>
2017-11-25 16:25:17 +0200

    .

--- LearningSystem.Services/Admin/IAdminService.cs
+++ LearningSystem.Services/Admin/IAdminService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LearningSystem.Services.Admin
+﻿namespace LearningSystem.Services.Admin
 {
     public interface IAdminService
     {
