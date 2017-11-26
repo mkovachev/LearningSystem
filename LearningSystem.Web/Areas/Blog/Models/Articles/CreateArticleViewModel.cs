dfc05ce718644b7750bb2cba260ee378d8d10c34
mkovachev <martin.kovachev@outlook.com>
2017-11-26 13:14:44 +0200

    create article view added

---
+++ LearningSystem.Web/Areas/Blog/Models/Articles/CreateArticleViewModel.cs
@@ -0,0 +1,20 @@
+﻿using LearningSystem.Data;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LearningSystem.Web.Areas.Blog.Models.Articles
+{
+    public class CreateArticleViewModel
+    {
+        [Required]
+        [MaxLength(DataConstants.TitleMaxLength)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(5000)]
+        public string Content { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+    }
+}
