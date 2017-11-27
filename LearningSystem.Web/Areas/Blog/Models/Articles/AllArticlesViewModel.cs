68f4bf328a89026b4b1eb1b493c311f67318e426
mkovachev <martin.kovachev@outlook.com>
2017-11-27 14:02:40 +0200

    pagination completed

--- LearningSystem.Web/Areas/Blog/Models/Articles/AllArticlesViewModel.cs
+++ LearningSystem.Web/Areas/Blog/Models/Articles/AllArticlesViewModel.cs
@@ -11,12 +11,12 @@ namespace LearningSystem.Web.Areas.Blog.Models.Articles
 
         public int TotalArticles { get; set; }
 
-        public int TotalPage => (int)Math.Ceiling((double)this.TotalArticles / ServiceConstants.AllArticlesPageSize);
+        public int TotalPages => (int)Math.Ceiling((double)this.TotalArticles / ServiceConstants.AllArticlesPageSize);
 
         public int CurrentPage { get; set; }
 
         public int PreviuosPage => this.CurrentPage == 1 ? 1 : this.CurrentPage - 1;
 
-        public int LastPage => this.CurrentPage == this.TotalPage ? this.TotalPage : this.CurrentPage + 1;
+        public int NextPage => this.CurrentPage == this.TotalPages ? this.TotalPages : this.CurrentPage + 1;
     }
 }
