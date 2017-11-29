4dee50f1ebf4b9378fc94ca473f65df6ad855224
mkovachev <martin.kovachev@outlook.com>
2017-11-29 10:04:54 +0200

    trainer link added trainer contoroller added

---
+++ LearningSystem.Web/Controllers/TrainerController.cs
@@ -0,0 +1,15 @@
+﻿using LearningSystem.Web.Infrastructure;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningSystem.Web.Controllers
+{
+    [Authorize(Roles = WebConstants.TrainerRole)]
+    public class TrainerController : Controller
+    {
+        public IActionResult Courses()
+        {
+            return View();
+        }
+    }
+}
