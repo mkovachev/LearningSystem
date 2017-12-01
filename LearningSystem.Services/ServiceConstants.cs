34bda3af638c7603df0932ae796be5bdc6f635b7
mkovachev <martin.kovachev@outlook.com>
2017-12-01 19:54:33 +0200

    grade un profile not working

--- LearningSystem.Services/ServiceConstants.cs
+++ LearningSystem.Services/ServiceConstants.cs
@@ -6,11 +6,12 @@
 
         public const string PdfCertinficateFormat = @"
               <h1>Certificate</h1>
-              <h2>{3}- Grade {4}</h2>
+              <h2>for {3}</h2>
+              <h2>Graduated with Grade {4}</h2>
               <br />
               <h2>{0} Course</h2>
-              <h3>{1} - {2}</h3>
+              <h4>{1} - {2}</h4>
               <h4>Signed by {5}</h4>
-              <h5>{6}</h5>";
+              <h5>Issued date: {6}</h5>";
     }
 }
